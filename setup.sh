6baa2ddb015e5478e94af241262b4b30449b12c5
vim <150005932@qq.com>
2016-02-25 16:56:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ cd ~/ && git clone https://github.com/preedom/vim.git
 mv -f ~/.vim ~/.vim_old
 mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
-mv -f ~/.vim/.vimrc ~/
+mv -f ~/.vim/vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "Lee正在努力为您安装bundle程序" > Lee
 echo "安装完毕将自动退出" >> Lee
