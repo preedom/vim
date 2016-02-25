3a2a75a811198c7e5af2e7b3ea817f28f9362cab
vim <150005932@qq.com>
2016-02-25 16:24:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,9 +21,9 @@ mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "ma6174正在努力为您安装bundle程序" > ma6174
-echo "安装完毕将自动退出" >> ma6174
-echo "请耐心等待" >> ma6174
-vim ma6174 -c "BundleInstall" -c "q" -c "q"
-rm ma6174
+echo "Lee正在努力为您安装bundle程序" > Lee
+echo "安装完毕将自动退出" >> Lee
+echo "请耐心等待" >> Lee
+vim Lee -c "BundleInstall" -c "q" -c "q"
+rm Lee
 echo "安装完成"
