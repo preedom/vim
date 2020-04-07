f7a0e6caf4f5e996298fe9c6995478ec344091fb
vim <150005932@qq.com>
2020-04-07 17:07:47 +0800

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl -Lo /etc/yum.repos.d/_copr_ibotty-prometheus-exporters.repo https://copr.fedorainfracloud.org/coprs/ibotty/prometheus-exporters/repo/epel-7/ibotty-prometheus-exporters-epel-7.repo
 yum install node_exporter -y
-echo 'OPTIONS="--collector.textfile.directory /var/lib/node_exporter/textfile_collector --web.listen-address=:16001"' > /etc/sysconfig/node_exporter
+echo 'OPTIONS="--collector.textfile.directory /var/lib/node_exporter/textfile_collector --web.listen-address=:6002"' > /etc/sysconfig/node_exporter
 systemctl start node_exporter
 netstat -ntulp|grep node_exporter
-iptables -A INPUT -s 49.4.26.245 -p tcp --dport 16001 -j ACCEPT
-iptables -A INPUT -p tcp --dport 16001 -j DROP
+iptables -A INPUT -s 49.4.26.245 -p tcp --dport 6002 -j ACCEPT
+iptables -A INPUT -p tcp --dport 6002 -j DROP
